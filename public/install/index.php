998defe0ecb60a38e41147540d7e94e06ab88379
刘孙全 <3206883466@qq.com>
2021-11-18 17:12:40 +0800

    open

--- public/install/index.php
+++ public/install/index.php
@@ -354,6 +354,16 @@ switch ($step) {
         @touch('./install.lock');
         exit();
 }
+//读取版本号
+function getversion(){
+    $version_arr = [];
+    $curent_version = @file(dirname(dirname(__DIR__)).'/.version');
+    foreach ($curent_version as $val){
+        list($k,$v)=explode('=',$val);
+        $version_arr[$k]=trim($v);
+    }
+    return $version_arr;
+}
 
 function testwrite($d)
 {
